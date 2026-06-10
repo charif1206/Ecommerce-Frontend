ae8f4b6df1d75174ed12ddcb831f3e287cee9684
charif <abedcharif027@gmail.com>
2026-06-10 15:29:25 +0100

    t

--- src/Axios/AxiosInstance.js
+++ src/Axios/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api", // Replace with your backend API URL
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api", // Configurable backend API URL
     withCredentials: true,
 });
 
